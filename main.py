255e85bd8e3809e071219eda552aed0ca4985e24
Federico <126673308+federico-capo@users.noreply.github.com>
2024-02-22 20:09:01 +0100

    0.16v

--- main.py
+++ main.py
@@ -1,80 +1,88 @@
+from tkinter import *
+from tkinter import filedialog, simpledialog
 from cryptography.fernet import Fernet
 import os
-import tkinter as tk
-from tkinter import filedialog, simpledialog
 
-def generate_key():
-    return Fernet.generate_key()
+def genera_chiave():
+    chiave = Fernet.generate_key()
+    result_label.config(text="Nuova chiave generata con successo.")
+    print("Nuova chiave generata con successo.")
+    print(f"Chiave: {chiave}")
+    salva_chiave(chiave)
+    return chiave
 
-def save_key(key, filename):
-    with open(filename, 'wb') as key_file:
-        key_file.write(key)
+#salva chiave
+def salva_chiave(chiave):
+    with open('chiave.key', 'wb') as file_chiave:
+        file_chiave.write(chiave)    
 
-def load_key(filename):
-    return open(filename, 'rb').read()
-
-def get_key_choice():
-    root = tk.Tk()
-    root.withdraw()
-
-    key_choice = tk.simpledialog.askstring("Scelta chiave", "Vuoi generare una nuova chiave o utilizzare una già esistente? (si/no): ").lower()
-    if key_choice == 'si':
-        return generate_key()
-    elif key_choice == 'no':
-        key_filename = filedialog.askopenfilename(title="Seleziona il file chiave")
-        if os.path.exists(key_filename):
-            return load_key(key_filename), key_filename  # Ritorna anche il percorso del file chiave
-        else:
-            print(f"Il file chiave '{key_filename}' non esiste. Generazione di una nuova chiave.")
-            return generate_key(), None
+#carica chiave
+def carica_chiave():
+    file_chiave= "chiave.key"
+    file_chiave = filedialog.askopenfilename(title="Seleziona il file chiave")
+    if os.path.exists(file_chiave):
+        result_label.config(text="Chiave caricata con successo.")
+        print("Chiave caricata con successo.")
+        print(f"File chiave: {file_chiave}")
+        return open(file_chiave, 'rb').read(), file_chiave
     else:
-        print("Scelta non valida. Generazione di una nuova chiave.")
-        return generate_key(), None
+        print(f"Il file chiave '{file_chiave}' non esiste. Generazione di una nuova chiave.")
+        return genera_chiave()
 
-def encrypt_file(key, filename, output_filename):
-    cipher = Fernet(key)
-    with open(filename, 'rb') as file:
-        data = file.read()
+#bottone cifra file compreso di carica chiave
+def cifra_file():
+    file = filedialog.askopenfilename(title="Seleziona il file da cifrare")
+    if os.path.exists(file):
+        with open(file, 'rb') as f:
+            data = f.read()
+        key, key_filename = carica_chiave()
+        cipher = Fernet(key)
         encrypted_data = cipher.encrypt(data)
-    with open(output_filename, 'wb') as file:
-        file.write(encrypted_data)
-
-def decrypt_file(key, filename, output_filename):
-    cipher = Fernet(key)
-    with open(filename, 'rb') as file:
-        encrypted_data = file.read()
-        decrypted_data = cipher.decrypt(encrypted_data)
-    with open(output_filename, 'wb') as file:
-        file.write(decrypted_data)
-
-def main():
-    key_filename = 'secret.key'
-    key, existing_key_filename = get_key_choice()
-
-    if existing_key_filename is not None:
-        # Se è stata scelta una chiave esistente, non chiedere il percorso di salvataggio
-        key_filename = existing_key_filename
+        with open(file, 'wb') as f:
+            f.write(encrypted_data)
+        result_label.config(text="File cifrato con successo.")
     else:
-        save_key(key, key_filename)
-        print("Chiave pronta.")
+        result_label.config(text="File non esistente.")
 
-    root = tk.Tk()
-    root.withdraw()
-
-    action_choice = tk.simpledialog.askstring("Scelta azione", "Vuoi cifrare o decifrare un file? (c/d): ").lower()
-
-    if action_choice == 'c':
-        input_filename = filedialog.askopenfilename(title="Seleziona il file da cifrare")
-        output_filename = filedialog.asksaveasfilename(title="Seleziona il file cifrato di output")
-        encrypt_file(key, input_filename, output_filename)
-        print("File cifrato con successo.")
-    elif action_choice == 'd':
-        decrypt_filename = filedialog.askopenfilename(title="Seleziona il file da decifrare")
-        decrypted_output_filename = filedialog.asksaveasfilename(title="Seleziona il file decifrato di output")
-        decrypt_file(key, decrypt_filename, decrypted_output_filename)
-        print("File decifrato con successo.")
+def decifra_file():
+    file = filedialog.askopenfilename(title="Seleziona il file da decifrare")
+    if os.path.exists(file):
+        with open(file, 'rb') as f:
+            data = f.read()
+        key, key_filename = carica_chiave()
+        cipher = Fernet(key)
+        decrypted_data = cipher.decrypt(data)
+        with open(file, 'wb') as f:
+            f.write(decrypted_data)
+        result_label.config(text="File decifrato con successo.")
     else:
-        print("Scelta non valida.")
+        result_label.config(text="File non esistente.")
 
-if __name__ == "__main__":
-    main()
+# GUI
+root = Tk()
+root.title("Cifratura/Decifratura File")
+root.geometry("410x150")
+root.configure(bg='#141212')
+
+# Etichetta per il risultato
+result_label = Label(root, text="", bg='#141212', fg='white')
+result_label.grid(row=4, column=0, columnspan=3)
+
+# #genera chiave
+btn_key = Button(root, text='Genera chiave', bd='6', command=genera_chiave,bg='blue', fg='#ffffff',font=("Arial", 9,'bold') )
+btn_key.grid(row=0, column=2, pady=10, padx=1, ipadx=20)
+
+# bottone decifra file
+btn_cifra = Button(root, text='Decifra file', bd='6', command=decifra_file, bg='green', fg='#ffffff',font=("Arial", 9,'bold') )
+btn_cifra.grid(row=0, column=0, pady=10, padx=10, ipadx=20)
+# bottone cifra file
+btn_cifra = Button(root, text='Cifra file', bd='6', command=cifra_file, bg='red', fg='#ffffff',font=("Arial", 9,'bold') )
+btn_cifra.grid(row=0, column=1, pady=10, padx=10, ipadx=20)
+
+# Bottone Esci
+btn_exit = Button(root, text='Esci', bd='6', command=root.destroy)
+btn_exit.grid(row=1, column=1, pady=10, padx=10, ipadx=20)
+
+
+# Esegui la finestra
+root.mainloop()
